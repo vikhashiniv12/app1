33448e60330c2cfbfbd1e5aed0354e650bf9b48f
vikhashiniv12 <164456445+vikhashiniv12@users.noreply.github.com>
2024-07-05 22:11:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,17 @@
-import os
+from flask import Flask
 import firebase_admin
 from firebase_admin import credentials, db
-from flask import Flask
 import matplotlib.pyplot as plt
 import io
 import base64
-import json
 
 app = Flask(__name__)
 
-# Read the service account key from environment variable
-encoded_service_account_key = os.getenv('ewogICJ0eXBlIjogInNlcnZpY2VfYWNjb3VudCIsCiAgInByb2plY3RfaWQiOiAidXNlci02NWM0MCIsCiAgInByaXZhdGVfa2V5X2lkIjogImNiMzcyMjU1MmEzOWQ5NDFkNmRlNTE1MDc1ODU0MmMwOGM1MzQ4Y2IiLAogICJwcml2YXRlX2tleSI6ICItLS0tLUJFR0lOIFBSSVZBVEUgS0VZLS0tLS0KTVlJRXdJQkFEQU5CZ2txYkhqa2tqaUdvejd3MEJBSAogIEFvSUJBRGFOQmdrcWlydU1ReTA0RUNUSGkKICBZcGRCUituOVJ4bEFydXhjT1B0SmtlVjRkaWdBZmhsb0RvVnQ1K2hTanowN0RZbllqZUpteEZ2a1FlYUlnL0N5WUcrQlpaTytDWm9Eb1Z0NStoU2pvN0RZbllqZ1BHWTNQeFJsU1k1eUgvM1ZqL1lPSG1VdEJ3cDlTNjhzdzdxOHhyMm5RYzdTbFdldWFWcFlBbXFsQmNOcDdsT3djb21kRjB4VWFrb0RrNHJMQnpyR3Z2ZEMxMzcyeDVTdXdOWUJxZk53Y09tZEE2U2xYZXVhVnBZQW1xbEIzYW5jT3hGTHVwT2p5VFVqSWd5Wk5qZ1BHR1lOT3B4UjFMU1k0NWUzRTEycm84U09icHdYZTZrczJ5ek1QZE54OHxHQU1CQUFCCUFBQ2d3WW5NM<|endoffile|>
-')
-
-if not encoded_service_account_key:
-    raise ValueError("The environment variable 'GOOGLE_APPLICATION_CREDENTIALS' is not set.")
-
-# Decode the Base64 string
-service_account_key = base64.b64decode(encoded_service_account_key).decode('utf-8')
-service_account_info = json.loads(service_account_key)
+# Replace with the actual path to your service account key JSON file
+service_account_key = "E:/serviceAccountKey.json"
 
 # Initialize Firebase with your credentials
-cred = credentials.Certificate(service_account_info)
+cred = credentials.Certificate(service_account_key)
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://user-65c40-default-rtdb.firebaseio.com'
 })
@@ -82,4 +72,3 @@ def plot_users():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
